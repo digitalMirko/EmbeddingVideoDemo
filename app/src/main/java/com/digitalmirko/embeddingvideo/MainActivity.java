bca67c46af8796ac11168bfc46f736c87064c2c2
Mirko Cukich <mirkocukich@gmail.com>
2020-01-05 16:26:50 -0600

    Program Complete

--- app/src/main/java/com/digitalmirko/embeddingvideo/MainActivity.java
+++ app/src/main/java/com/digitalmirko/embeddingvideo/MainActivity.java
@@ -3,6 +3,8 @@ package com.digitalmirko.embeddingvideo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.MediaController;
+import android.widget.VideoView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        VideoView videoView = (VideoView) findViewById(R.id.videoView);
+
+        videoView.setVideoPath("android.resource://" + getPackageName() + "/" + R.raw.deargod);
+
+        // video controls
+        MediaController mediaController = new MediaController(this);
+        // links to video view
+        mediaController.setAnchorView(videoView);
+        // allow to control video view
+        videoView.setMediaController(mediaController);
+
+        videoView.start();
     }
 }
